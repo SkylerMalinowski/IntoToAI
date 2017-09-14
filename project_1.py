73235cc080102bd22c3d2b7e523a785c0376ecb5
Skyler Malinowski <skyler.malinowski@gmail.com>
2017-09-14 00:43:33 -0400

    Update project_1.py
    WIP file update

--- project_1.py
+++ project_1.py
@@ -1,3 +1,11 @@
+# **************************************************************************** #
+# Skyler Malinowski [ som12 ]
+# Andrew Dos Reis [ ad1005 ]
+# Project 1
+# CS 440
+#
+# **************************************************************************** #
+
 # Sys and Opt for CLI arguments and flags
 import sys, getopt
 # NumPy for math functions and data structures
@@ -8,53 +16,91 @@ import matplotlib.pyplot as plt
 import math
 # Random for random numbers
 import random
+# Node and RenderTree for making and displaying trees
+#from anytree import Node, RenderTree, AsciiStyle
+#from anytree.dotexport import RenderTreeGraph
 
-# Task 1
+
+# Task 1  **********************************************************************
+
 def checkArgv(argv):
 	for validArgv in ['5','7','9','11']:
 		if argv[1] == validArgv:
 			return True
 	
 	return False
-			
-# Task 1
+
+
 def makeMatrix(size):
 	
 	n = int(size)
 	
 	# makes n by n matrix
 	matrix = np.zeros( shape=(n,n) )
+	visitMat = np.zeros( shape=(n,n) )
 	
 	# populates n by n matrix
 	for row in range(n):
 		for col in range(n):
-			matrix[row,col] = random.randint(1,n-1)
-	
-	matrix[n-1,n-1] = 0  # goal
+			Max = max(n-1-row,row-n-1,n-1-col,col-n-1)
+			if Max == 0:
+				matrix[row,col] = 0
+			else:
+				matrix[row,col] = random.randint(1,Max)
 	
 	# display n by n matrix
-	print matrix
+	print(matrix)
+	return matrix, visitMat
 
-# Task 2
-def validMove():
-	pass
 
-# Task 2
-def evalMatrix():
+# Task 2  **********************************************************************
+
+class Node(object):
+	def __init__(self,row,col,data):
+		self.row = row
+		self.col = col
+		self.data = data
+		self.children = []
+	
+	def add_child(self,obj):
+		self.children.append(obj)
+
+
+def buildTree(matrix,visitMat,size):
+	n = int(size)
+	
+	curr_row = 0
+	curr_col = 0
+	root = Node(curr_row,curr_col,matrix[curr_row,curr_col])
+	visitMat[curr_row,curr_col] = 1
+	
+	#while(True):
+		# check if legal move and has not been visited
+	
+	#print("\n",RenderTree(root, style=AsciiStyle()))
+	#RenderTreeGraph(root).to_dotfile("tree.dot")
+	# run to convert .dot to .pdf
+	# $ dot -Tpdf tree.dot -o tree.pdf
+
+
+def evalMatrix(matrix,visitMat):
 	pass # code
 
-# main
+
+# Main  ************************************************************************
 def main(argv):
 	# Task 1
-	makeMatrix(argv[1])
+	matrix,visitMat = makeMatrix(argv[1])
 	
 	# Task 2
-	evalMatrix()
-	
+	Tree = buildTree(matrix,visitMat,argv[1])
+	evalMatrix(matrix,visitMat)
+
+
 # run main module if not imported
 if __name__ == "__main__":
 	if checkArgv(sys.argv) == False:
-		print "arguement error: not in domain [5,7,9,11]"
+		print("arguement error: not in domain [5,7,9,11]")
 	
 	else:
 		main(sys.argv)
