66b57269f8ee49d4ac07ae76439578378dc4174f
Skyler Malinowski <skyler.malinowski@gmail.com>
2017-09-15 13:59:06 -0400

    Update project_1.py
    Finished tree building and tree print. Next to-do is evaluation function.

--- project_1.py
+++ project_1.py
@@ -3,7 +3,6 @@
 # Andrew Dos Reis [ ad1005 ]
 # Project 1
 # CS 440
-#
 # **************************************************************************** #
 
 # Sys and Opt for CLI arguments and flags
@@ -16,10 +15,6 @@ import matplotlib.pyplot as plt
 import math
 # Random for random numbers
 import random
-# Node and RenderTree for making and displaying trees
-#from anytree import Node, RenderTree, AsciiStyle
-#from anytree.dotexport import RenderTreeGraph
-
 
 # Task 1  **********************************************************************
 
@@ -48,39 +43,118 @@ def makeMatrix(size):
 			else:
 				matrix[row,col] = random.randint(1,Max)
 	
-	# display n by n matrix
+	# debug
 	print(matrix)
+	
 	return matrix, visitMat
 
 
 # Task 2  **********************************************************************
 
+# Tree
 class Node(object):
 	def __init__(self,row,col,data):
+		self.name = "(" + str(row) + "," + str(col) + ")"
 		self.row = row
 		self.col = col
 		self.data = data
-		self.children = []
+		self.items = []
+	
+	def __repr__(self):
+		return self.name
 	
 	def add_child(self,obj):
-		self.children.append(obj)
+		self.items.append(obj)
+	
+	def get_name(self):
+		return self.name
+	
+	def get_children(self):
+		return self.items
 
 
-def buildTree(matrix,visitMat,size):
+class Queue():
+	def __init__(self):
+		self.items = []
+	
+	def isEmpty(self):
+		return self.items == []
+	
+	def enqueue(self, item):
+		self.items.insert(0,item)
+	
+	def dequeue(self):
+		return self.items.pop()
+	
+	def size(self):
+		return len(self.items)
+
+
+def printTree(root,lv=0):
+	print( lv * '  ', root.get_name() )
+	
+	children = root.get_children()
+	lv += 1
+	
+	for child in children:
+		printTree(child,lv)
+
+
+def validMoves(matrix,visitMat,curr):
+	valid_list = []
+	
+	test = int(curr[1] + curr[2])
+	if test < len(matrix) and visitMat[curr[0],test] == 0:  # Right
+		valid_list.append([ curr[0],test,matrix[curr[0],test] ])
+	
+	test = int(curr[0] + curr[2])
+	if test < len(matrix) and visitMat[test,curr[1]] == 0:  # Down
+		valid_list.append([ test,curr[1],matrix[test,curr[1]] ])
+	
+	test = int(curr[1] - curr[2])
+	if test >= 0 and visitMat[test,curr[0]] == 0:  # Left
+		valid_list.append([ curr[0],test,matrix[curr[0],test] ])
+	
+	test = int(curr[0] - curr[2])
+	if test >= 0 and visitMat[test,curr[1]] == 0:  # Up
+		valid_list.append([ test,curr[1],matrix[test,curr[1]] ])
+	
+	# debug
+	#print(valid_list)
+	
+	return valid_list
+
+
+def buildTree(matrix,visitMat,size,row,col):
 	n = int(size)
+	Q = Queue()
 	
-	curr_row = 0
-	curr_col = 0
-	root = Node(curr_row,curr_col,matrix[curr_row,curr_col])
-	visitMat[curr_row,curr_col] = 1
+	root = Node(row,col,matrix[row,col])  # root tree node
+	Q.enqueue([row,col,matrix[row,col]])  # row,col,jump
+	visitMat[row,col] = 1
 	
-	#while(True):
-		# check if legal move and has not been visited
+	curr_data = None  # current node data
+	curr_node = root
+	while( Q.isEmpty() == False ):
+		prev_data = curr_data  # previous node data
+		prev_node = curr_node
+		curr_data = Q.dequeue()
+		
+		# check valid for children (move,visit)
+		valid_moves = validMoves(matrix,visitMat,curr_data)
+		
+		# tree,queue,visit
+		for child in valid_moves:
+			curr_node = Node(child[0],child[1],child[2])
+			prev_node.add_child(curr_node)
+			Q.enqueue(child)
+			visitMat[child[0],child[1]] = 1
 	
-	#print("\n",RenderTree(root, style=AsciiStyle()))
-	#RenderTreeGraph(root).to_dotfile("tree.dot")
-	# run to convert .dot to .pdf
-	# $ dot -Tpdf tree.dot -o tree.pdf
+	# debug
+	#printTree(root)
+	#print(visitMat)
+	
+	return root
 
 
 def evalMatrix(matrix,visitMat):
@@ -93,7 +167,7 @@ def main(argv):
 	matrix,visitMat = makeMatrix(argv[1])
 	
 	# Task 2
-	Tree = buildTree(matrix,visitMat,argv[1])
+	tree = buildTree(matrix,visitMat,argv[1],0,0)
 	evalMatrix(matrix,visitMat)
 
 
